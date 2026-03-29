abb05d9f8497e5f68a7da23669f80217fc9d4d3b
Richard Zhao <richardzhaoforwork@gmail.com>
2026-03-29 18:24:34 +1300

    Version 1.3
    - Fixed page switching not resulting in top position issue

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import { Header } from './Header'
 import { Footer } from './Footer'
 
@@ -7,6 +9,12 @@ interface LayoutProps {
 }
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [pathname])
+
   return (
     <div className='font-inter bg-brand-blue flex min-h-dvh flex-col font-light text-slate-800'>
       <Header />
